e546384e30ffe17dbc8a178bffe8aa9036047a3b
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-10-22 20:05:24 -0300

    init provider and data

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  # backend "s3" {
+  # bucket = "${var.company}-${var.project}-${var.environment}"
+  # key    = "terraform-test.tfstate"
+  # region = "var.aws_region"
+  # }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
