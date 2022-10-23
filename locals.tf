af3463aa2361b5714a6e57459861a23416f2d954
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-10-23 20:33:26 -0300

    processing of subnets

--- locals.tf
+++ locals.tf
@@ -2,6 +2,21 @@ locals {
   common_tags = {
     company      = var.company
     project      = "${var.company}-${var.project}"
-    environment  = "${var.project}-${var.environment}"
+    enviroment  = "${var.project}-${var.enviroment}"
   }
+}
+
+locals {
+  subnets_app = sort([for subnet in var.vpc_configuration.subnets : subnet.name if subnet.public == false])
+  subnets_data  = sort([for subnet in var.vpc_configuration.subnets_data : subnet.name if subnet.public == false])
+  public_subnets  = sort([for subnet in var.vpc_configuration.subnets : subnet.name if subnet.public == true])
+  azs             = sort(slice(data.aws_availability_zones.available.zone_ids, 0, length(local.subnets_app)))
+  subnet_pairs    = zipmap(local.subnets_app, local.public_subnets)
+  subnet_pairs_app_data    = zipmap(local.subnets_app, local.public_subnets)
+
+  az_pairs = merge(
+    zipmap(local.public_subnets, local.azs),
+    zipmap(local.subnets_app, local.azs),
+    zipmap(local.subnets_data, local.azs)
+  )
 }
