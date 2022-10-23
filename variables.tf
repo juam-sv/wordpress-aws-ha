351d886eb90ae348761103052f7d463678261cf3
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-10-23 20:33:12 -0300

    add of subnet for data

--- variables.tf
+++ variables.tf
@@ -15,3 +15,56 @@ variable "enviroment" {
   description = "Environment name for resource tagging"
   default = "staging"
 }
+
+variable "vpc_configuration" {
+  type = object({
+    cidr_block = string
+    subnets = list(object({
+      name       = string
+      cidr_block = string
+      public     = bool
+    }))
+    subnets_data = list(object({
+      name       = string
+      cidr_block = string
+      public     = bool
+    }))
+  })
+  default = {
+    cidr_block = "10.0.0.0/16"
+    subnets = [
+      {
+        name       = "app-a"
+        cidr_block = "10.0.0.0/19"
+        public     = false
+      },
+      {
+        name       = "app-b"
+        cidr_block = "10.0.32.0/19"
+        public     = false
+      },
+      {
+        name       = "public-a"
+        cidr_block = "10.0.128.0/19"
+        public     = true
+      },
+      {
+        name       = "public-b"
+        cidr_block = "10.0.160.0/19"
+        public     = true
+      }
+    ]
+    subnets_data = [
+      {
+        name       = "data-a"
+        cidr_block = "10.0.64.0/19"
+        public     = false
+      },
+      {
+        name       = "data-b"
+        cidr_block = "10.0.192.0/19"
+        public     = false
+      }
+    ]
+  }
+}
