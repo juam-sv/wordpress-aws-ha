2370cba2356c7a04731c5848aecb284cd303a391
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-10-23 21:34:24 -0300

    routes, nat and gateways for subnets

--- locals.tf
+++ locals.tf
@@ -12,7 +12,7 @@ locals {
   public_subnets  = sort([for subnet in var.vpc_configuration.subnets : subnet.name if subnet.public == true])
   azs             = sort(slice(data.aws_availability_zones.available.zone_ids, 0, length(local.subnets_app)))
   subnet_pairs    = zipmap(local.subnets_app, local.public_subnets)
-  subnet_pairs_app_data    = zipmap(local.subnets_app, local.public_subnets)
+  subnet_pairs_app_data    = zipmap(local.subnets_app, local.subnets_data)
 
   az_pairs = merge(
     zipmap(local.public_subnets, local.azs),
--- network.tf
+++ network.tf
@@ -10,6 +10,7 @@ resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 }
 
+#------------------- SUBNETS CREATION ------------------------------------------------------
 resource "aws_subnet" "this" {
   for_each = { for subnet in var.vpc_configuration.subnets : subnet.name => subnet }
 
@@ -30,4 +31,55 @@ resource "aws_subnet" "this_data" {
   map_public_ip_on_launch = each.value.public
 
   tags = local.common_tags
+}
+
+#------------------- NAT GATEWAY CREATION --------------------------------------------------
+resource "aws_nat_gateway" "this" {
+  for_each = toset(local.subnets_app)
+
+  allocation_id = aws_eip.nat_gateway[each.value].id
+  subnet_id     = aws_subnet.this[local.subnet_pairs[each.value]].id
+}
+
+resource "aws_eip" "nat_gateway" {
+  for_each = toset(local.subnets_app)
+  vpc      = true
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+#------------------- ROUTES CREATION --------------------------------------------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route" "internet_gateway" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each       = toset(local.public_subnets)
+  subnet_id      = aws_subnet.this[each.value].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  for_each = toset(local.subnets_app)
+  vpc_id   = aws_vpc.this.id
+}
+
+resource "aws_route" "nat_gateway" {
+  for_each = toset(local.subnets_app)
+
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.private[each.value].id
+  nat_gateway_id         = aws_nat_gateway.this[each.value].id
+}
+
+resource "aws_route_table_association" "private" {
+  for_each       = toset(local.subnets_app)
+  subnet_id      = aws_subnet.this[each.value].id
+  route_table_id = aws_route_table.private[each.value].id
 }
