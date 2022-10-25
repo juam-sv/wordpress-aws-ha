cce5bdef05d1d26071a32cd107df555466789bee
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-10-25 15:50:46 -0300

    asg and lb

--- asg.tf
+++ asg.tf
@@ -1,3 +1,4 @@
+#------------------- AUTO SCALING GROUP -------------------------------------- 
 resource "aws_autoscaling_group" "asg_wordpress" {
   name             = "${aws_launch_configuration.lc_wordpress.name}-asg"
   min_size         = 1
@@ -24,4 +25,50 @@ resource "aws_autoscaling_group" "asg_wordpress" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+#------------------- AUTO SCALING POLICY-----------------------------------------
+resource "aws_autoscaling_policy" "wordpress_asg_up" {
+  name                   = "wordpress_asg_up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.asg_wordpress.name
+}
+resource "aws_cloudwatch_metric_alarm" "wordpress_cpu_alarm_up" {
+  alarm_name          = "wordpress_cpu_alarm_up"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "70"
+  dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.asg_wordpress.name}"
+  }
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.wordpress_asg_up.arn}"]
+}
+resource "aws_autoscaling_policy" "wordpress_asg_down" {
+  name                   = "wordpress_asg_down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.asg_wordpress.name
+}
+resource "aws_cloudwatch_metric_alarm" "wordpress_cpu_alarm_down" {
+  alarm_name          = "wordpress_cpu_alarm_down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "30"
+  dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.asg_wordpress.name}"
+  }
+  alarm_description = "This metric monitor EC2 instance CPU utilization"
+  alarm_actions     = ["${aws_autoscaling_policy.wordpress_asg_down.arn}"]
 }
--- data.sh
+++ data.sh
@@ -1,8 +1,5 @@
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
-sudo chmod 666 /var/run/docker.sock
-docker pull juamsv/app-html:v1
-docker run -d -p 80:80 juamsv/app-html:v1
+#! /bin/bash
+sudo amazon-linux-extras install -y nginx1
+sudo service nginx start
+sudo rm /usr/share/nginx/html/index.html
+echo '<html><head><title>Madra Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
