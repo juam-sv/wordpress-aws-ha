6403028a1bd9a6a096940cd6b308b7b801433ada
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-10-24 21:42:53 -0300

    elb pub subnet

---
+++ elb.tf
@@ -0,0 +1,24 @@
+resource "aws_elb" "lb_wordpress" {
+  name = "lb-wordpress"
+  security_groups = [
+    "${aws_security_group.sg_http_and_ssh.id}"
+  ]
+  subnets = [
+    "${aws_subnet.this["public-a"].id}",
+    "${aws_subnet.this["public-b"].id}"
+  ]
+  cross_zone_load_balancing = true
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:80/"
+  }
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "80"
+    instance_protocol = "http"
+  }
+}
