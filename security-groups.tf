de072cd9da804f80a52f00e799c44b410a02fd7c
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-10-26 18:43:44 -0300

    add of efs sg and limite ssh and http ports for wordpress

--- security-groups.tf
+++ security-groups.tf
@@ -1,25 +1,17 @@
 resource "aws_security_group" "sg_http_and_ssh" {
   name        = "http_and_ssh"
-  description = "Open http and ssh ports"
+  description = "Open http, https and ssh ports"
   vpc_id      = aws_vpc.this.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.ec2_sg_ingress_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["${var.vpc_configuration.cidr_block}"] #verificar
+    }
   }
 
   egress {
@@ -29,3 +21,26 @@ resource "aws_security_group" "sg_http_and_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# -------------------- EFS SECURITY GROUP --------------------
+
+resource "aws_security_group" "efs_sg" {
+  name        = "efs_sg"
+  description = "Opening EFS port"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_http_and_ssh.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
