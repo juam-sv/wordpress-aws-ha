64f20da551e5e57405c24b29039fe5bccde3c2bb
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-10-26 21:48:49 -0300

    add sg for db

--- security-groups.tf
+++ security-groups.tf
@@ -44,3 +44,26 @@ resource "aws_security_group" "efs_sg" {
   }
 }
 
+# -------------------- RDS SECURITY GROUP --------------------
+resource "aws_security_group" "sg_db_cluster" {
+  name        = "sg_db_cluster"
+  description = "Open MySQL port 3306 for EC2 instances"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port       = var.db_port
+    to_port         = var.db_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_http_and_ssh.id]
+    cidr_blocks     = ["${aws_subnet.this["public-a"].id}", "${aws_subnet.this["public-b"].id}"]
+
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
